b3d1f47db11034c8774bab206ce02d84f7bc0d22
Mahil-SA <mahilsamson2016@gmail.com>
2024-07-24 20:42:21 +0530

    EXP1-080.cpp

---
+++ EXP1-080.cpp
@@ -0,0 +1,58 @@
+//EXP 1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+string a;
+
+    
+    cout<<"Enter your name  ";
+    getline(cin,a);
+    cout<<"Hello "<<a;
+    return 0;
+} 
+
+
+//ADDITION
+#include <i/ostream>
+using namespace std;
+int main()
+{ int a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a+b;
+cout << "the sum is "<<c;
+return 0;
+} 
+
+
+//DIVISION
+#include <iostream>
+using namespace std;
+int main()
+{ float a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a/b;
+cout << "the QUOTIENT is "<<c;
+return 0;
+}
+
+//MULTIPLICATION
+#include <iostream>
+using namespace std;
+int main()
+{ float a,b,c;
+cout << "enter first number ";
+cin >> a;
+cout << "enter the second number ";
+cin >> b;
+c=a*b;
+cout << "the RESULT is "<<c;
+return 0;
+} 
